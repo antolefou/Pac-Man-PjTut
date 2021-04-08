6ee93cb361e2d48565eea460681c0b6042376c15
nmention <noe.mention@protonmail.com>
2021-04-08 21:16:59 +0200

    Création de credit.fxml et de propos.fxml + switch de scene

--- Projet-Tut-S2/src/sample/Controller.java
+++ Projet-Tut-S2/src/sample/Controller.java
@@ -7,17 +7,11 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.application.Platform;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.SQLOutput;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Controller implements EventHandler<KeyEvent> {
     @FXML public sample.Pacman Pacman;
@@ -92,4 +86,32 @@ public class Controller implements EventHandler<KeyEvent> {
         primaryStage.show();
         root.requestFocus();
     }
+    @FXML
+    public void switchToSceneCredits(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("credit.fxml"));
+        Parent root = loader.load();
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setTitle("Pac-Man");
+        Controller controller = loader.getController();
+        root.setOnKeyPressed(controller);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        root.requestFocus();
+    }
+    @FXML
+    public void switchToScenePropos(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("propos.fxml"));
+        Parent root = loader.load();
+        Stage primaryStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        primaryStage.setTitle("Pac-Man");
+        Controller controller = loader.getController();
+        root.setOnKeyPressed(controller);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.sizeToScene();
+        primaryStage.show();
+        root.requestFocus();
+    }
+
+
 }
