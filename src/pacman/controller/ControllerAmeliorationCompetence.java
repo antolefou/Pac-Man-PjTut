0928dcd351b92cbd05f4ad4e338eda44a0999fb4
Cyberooo <julien.erhard@edu.univ-fcomte.fr>
2021-06-09 18:29:02 +0200

    ajout points

--- src/pacman/controller/ControllerAmeliorationCompetence.java
+++ src/pacman/controller/ControllerAmeliorationCompetence.java
@@ -3,15 +3,21 @@ package pacman.controller;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.text.Text;
 import pacman.model.Utilisateur;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class ControllerAmeliorationCompetence extends Controller {
     public Utilisateur utilisateur;
     public Text description;
+    public ImageView pointTirer;
+    public ImageView pointFreeze;
+    public ImageView pointTeleporteur;
 
     public ControllerAmeliorationCompetence() {
         utilisateur = new Utilisateur();
@@ -20,21 +26,34 @@ public class ControllerAmeliorationCompetence extends Controller {
     @FXML
     public void initialize() {
         description.setText("Veuillez améliorer vos compétences, ou cliquer sur valider");
+        if (utilisateur.niveauCompetenceTirer>-1) pointTirer.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacman/ressources/image/Ecran_Competence/groupePoint" + utilisateur.niveauCompetenceTirer + "Vide.png"))));
+        else pointTirer.setImage(null);
+        if (utilisateur.niveauCompetenceFreeze>-1)pointFreeze.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacman/ressources/image/Ecran_Competence/groupePoint" + utilisateur.niveauCompetenceFreeze + "Vide.png"))));
+        else pointFreeze.setImage(null);
+        if (utilisateur.niveauCompetenceTeleporteur>-1)pointTeleporteur.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/pacman/ressources/image/Ecran_Competence/groupePoint" + utilisateur.niveauCompetenceTeleporteur + "Vide.png"))));
+        else pointTeleporteur.setImage(null);
     }
 
     public void ameliorationPouvoir(ActionEvent actionEvent) {
+        String image;
         switch (((Node) actionEvent.getSource()).getId()) {
             case "tirer":
                 if (utilisateur.niveauCompetenceTirer<3) utilisateur.niveauCompetenceTirer ++;
                 description.setText(utilisateur.tabCompetence[utilisateur.niveauCompetenceTirer+1][0]);
+                image = "/pacman/ressources/image/Ecran_Competence/groupePoint" + (utilisateur.niveauCompetenceTirer) + "Vide.png";
+                if (utilisateur.niveauCompetenceTirer>-1) pointTirer.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream(image))));
                 break;
             case "freeze":
                 if (utilisateur.niveauCompetenceFreeze<3) utilisateur.niveauCompetenceFreeze ++;
                 description.setText(utilisateur.tabCompetence[utilisateur.niveauCompetenceFreeze+1][4]);
+                image = "/pacman/ressources/image/Ecran_Competence/groupePoint" + (utilisateur.niveauCompetenceFreeze) + "Vide.png";
+                if (utilisateur.niveauCompetenceFreeze>-1) pointFreeze.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream(image))));
                 break;
             case "teleporteur":
                 if (utilisateur.niveauCompetenceTeleporteur<3) utilisateur.niveauCompetenceTeleporteur ++;
                 description.setText(utilisateur.tabCompetence[utilisateur.niveauCompetenceTeleporteur+1][8]);
+                image = "/pacman/ressources/image/Ecran_Competence/groupePoint" + (utilisateur.niveauCompetenceTeleporteur) + "Vide.png";
+                if (utilisateur.niveauCompetenceTeleporteur>-1) pointTeleporteur.setImage(new Image(Objects.requireNonNull(getClass().getResourceAsStream(image))));
                 break;
         }
     }
